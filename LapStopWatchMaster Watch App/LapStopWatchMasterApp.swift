13146990c266932e1abd1df7149b3b376ee05387
Alun King <alun.king1@southwales.ac.uk>
2024-11-29 12:14:03 +0000

    Initial Commit

---
+++ LapStopWatchMaster Watch App/LapStopWatchMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LapStopWatchMasterApp.swift
+//  LapStopWatchMaster Watch App
+//
+//  Created by Alun King on 29/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct LapStopWatchMaster_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
